ff258fc2c681b751c0abfbf7951a2a10e7832437
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-01-03 03:55:26 +0100

    Fixes in servers

--- modules/pastafari/admin/servers.py
+++ modules/pastafari/admin/servers.py
@@ -5,13 +5,28 @@ from paramecio.citoplasma.mtemplates import ptemplate
 from paramecio.citoplasma.lists import SimpleList
 from modules.pastafari.models import servers
 from paramecio.citoplasma.urls import make_url
+from paramecio.citoplasma.httputils import GetPostFiles
 
 def admin(t):
     
     t=ptemplate(__file__)
     
+    GetPostFiles.obtain_get()
+    
+    GetPostFiles.get['op']=GetPostFiles.get.get('op', '')
+    
     server=servers.ServerModel()
     url=make_url(config.admin_folder+'/pastafari/servers')
-    server_list=SimpleList(server, url, t)
     
-    return t.load_template('servers.phtml', server_list=server_list, url=url)
+    if GetPostFiles.get['op']=='add_new_server':
+    
+        server.create_forms()
+    
+        return ""
+        pass
+    
+    else:
+    
+        server_list=SimpleList(server, url, t)
+    
+        return t.load_template('servers.phtml', server_list=server_list, url=url)
